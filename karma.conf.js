7d0dcc10714f24f7edf706a99c4eed39f6e712c0
Victor Narcizo <vnarcizo@gmail.com>
2021-09-02 22:21:49 -0300

    Config server

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/AngularGraphQLClient'),
+      dir: require('path').join(__dirname, './coverage/pi-grupo-003'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularGraphQLClient'`, () => {
+  it(`should have as title 'pi-grupo-003'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularGraphQLClient');
+    expect(app.title).toEqual('pi-grupo-003');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularGraphQLClient app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pi-grupo-003 app is running!');
   });
 });
